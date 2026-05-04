dc620f05e37e8d8ebd09b5b19628835ea9de89d5
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-05-04 13:01:39 +0530

    Add files via upload

---
+++ question1/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Child from './components/Child';
+
+const App = () => {
+
+  const name = "Aditya";
+  const age = 21;
+  const hobby = ["Coding", "Cricket", "Music"];
+  return (
+    <div>
+      <h1>Parent Component</h1>
+
+      <Child name={name} age={age} hobby={hobby} />
+    </div>
+  )
+}
+
+export default App
+
